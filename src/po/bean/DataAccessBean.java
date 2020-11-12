e7f748ab908e217c5dde82fcf70612e71d9b8c1b
tomomo086 <74095374+tomomo086@users.noreply.github.com>
2020-11-13 00:52:50 +0900

    賞味期限表示のSQLを変更
    or note like '%早%'を追加

--- src/po/bean/DataAccessBean.java
+++ src/po/bean/DataAccessBean.java
@@ -103,7 +103,7 @@ public class DataAccessBean {
 
 			// SQLの命令文
 			String sql = "select type,name,amount,buy,note from reizouko2 "
-					+ "where  gomi is null and note like '%期限%' order by buy asc";
+					+ "where  gomi is null and note like '%期限%'or note like '%早%' order by buy asc";
 
 			// コレクションをインスタンス化したrefListを作成
 			Collection<RefInfo> refsyoumiList = new ArrayList<RefInfo>();
